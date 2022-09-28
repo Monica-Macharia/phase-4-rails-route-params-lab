abafb40ed3c0001349f198660023b4d1f0e18aaf
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-28 07:10:12 +0300

    params in rails

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    number = Student.find_by(id: params[:id])
+    render json: number
+  end
 
 end
